c15589658abbf3c355c73052eb7ba95f7ee21c43
aravind1314 <aravindtone@gmail.com>
2023-04-28 12:13:22 +0530

    docker login changes

--- script-cmnds.sh
+++ script-cmnds.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-export URL=$1
-export IMAGE=$2
-export docker_user=$3
-export docker_pswd=$4
-
-docker login -u $docker_user -p $docker_pswd
-
 docker-compose -f docker-compose.yaml up --detach
 
 echo " docker compose successful"
