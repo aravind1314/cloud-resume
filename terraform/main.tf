d29754483add1f21e37ba973cef9c150433740ca
aravind1314 <aravindtone@gmail.com>
2023-05-04 09:27:51 +0530

    trfm/sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,6 +61,18 @@ resource "aws_security_group" "my_sg" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port = 0
     to_port = 0
