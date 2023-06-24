57054f8c60177007afd5f071e7a15ae0560ed996
aravind1314 <aravindtone@gmail.com>
2023-06-24 05:37:31 +0530

    constant ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,7 +89,7 @@ data "aws_ami" "latest_ami" {
   owners = ["amazon"]
   filter {
     name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230325"]
   }
 }
 
